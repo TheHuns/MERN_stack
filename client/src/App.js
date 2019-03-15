35b8834302392ec01671bfe9f8dc80daef125d6c
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-03-14 19:02:44 -0600

    redux boilerplate setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+
+import store from "./store";
+import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -7,9 +11,12 @@ import "./App.css";
 class App extends Component {
    render() {
       return (
-         <div className="App">
-            <AppNavbar />
-         </div>
+         <Provider store={store}>
+            <div className="App">
+               <AppNavbar />
+               <ShoppingList />
+            </div>
+         </Provider>
       );
    }
 }
