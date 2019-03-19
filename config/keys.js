947b382e50a0d288d0f0984a41b7ff2d812260dc
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-03-19 12:31:01 -0600

    user authentication route

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-   mongoURI:
-      "mongodb+srv://dehnger:Bayfield85%21@cluster0-cho1k.mongodb.net/MERN?retryWrites=true"
-};
---
+++ routes/api/auth.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+// Item model
+const User = require("../../models/User");
+
+// @route POST api/auth
+// @desc Register new user
+// @acess Public
+router.post("/", (req, res) => {
+   const { email, password } = req.body;
+
+   //    simple validation
+   if ((!email, !password)) {
+      return res.status(400).json({ msg: "Pleast enter all fields" });
+   }
+
+   //    check for existing user
+   User.findOne({ email }).then(user => {
+      if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+      // Validate password
+      bcrypt.compare(password, user.password).then(isMatch => {
+         if (!isMatch)
+            return res.status(400).json({ msg: "Invalid credentials" });
+
+         jwt.sign(
+            {
+               id: user.id
+            },
+            config.get("jwtSecret"),
+            { expiresIn: 3600 },
+            (err, token) => {
+               if (err) throw err;
+               res.json({
+                  token,
+                  user: {
+                     id: user.id,
+                     name: user.name,
+                     email: user.email
+                  }
+               });
+            }
+         );
+      });
+   });
+});
+
+module.exports = router;
